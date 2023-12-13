acf8098e731b75eadded5b50de719b1f2f7beebe
wnaiyakhu <wnaiyakhu@gmail.com>
2023-12-13 17:53:07 -0500

    added new notebook

--- app.py
+++ app.py
@@ -29,3 +29,14 @@ def predict():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def dashboard():
+    return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
